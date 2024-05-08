df495b2c4c43177e013c86c737fb40ad92e3e56e
Slx24s <SalahAhmed1904@outlook.com>
2024-05-08 22:30:34 +0100

    Added functions and modified selection tool function so that it properly selects tools and deselects colours

--- toolbox.js
+++ toolbox.js
@@ -1,90 +1,75 @@
-//container object for storing the tools. Functions to add new tools and select a tool
-function Toolbox() {
-
-	var self = this;
-
-	this.tools = [];
-	this.selectedTool = null;
-
-	var toolbarItemClick = function() {
-		//remove any existing borders
-		var items = selectAll(".sideBarItem");
-		for (var i = 0; i < items.length; i++) {
-			items[i].style('border', '0');
-		}
-
-		var toolName = this.id().split("sideBarItem")[0];
-		self.selectTool(toolName);
-
-		//call loadPixels to make sure most recent changes are saved to pixel array
-		loadPixels();
-	};
-
-	//add a new tool icon to the html page
-	var addToolIcon = function(icon, name) {
-		var sideBarItem = createDiv("<img src='" + icon + "'></div>");
-		sideBarItem.class("sideBarItem");
-		sideBarItem.id(name + "sideBarItem");
-		sideBarItem.parent("sidebar");
-		sideBarItem.mouseClicked(toolbarItemClick);
 
 
-	};
+function Toolbox(colourPalette) {
+    var self = this;
 
-	
-	//add a tool to the tools array
-	this.addTool = function(tool) {
-		//check that the object tool has an icon and a name
-		if (!tool.hasOwnProperty("icon") || !tool.hasOwnProperty("name")) {
-			alert("make sure your tool has both a name and an icon");
-		}
-		this.tools.push(tool);
-		addToolIcon(tool.icon, tool.name);
-		//if no tool is selected (ie. none have been added so far)
-		//make this tool the selected one.
-		if (this.selectedTool == null) {
-			this.selectTool(tool.name);
-		}
-	};
+    this.tools = [];
+    this.selectedTool = null;
+    this.colourPalette = colourPalette;  // Store the reference to ColourPalette
 
-	this.resetDrawingState = function() {
-		stroke(0); // Default to black
-		strokeWeight(1); // Default stroke weight
-		// Add other default settings
-	};
+    var toolbarItemClick = function() {
+        var items = selectAll(".sideBarItem");
+        for (var i = 0; i < items.length; i++) {
+            items[i].style('border', '0');
+        }
 
-	this.selectTool = function(toolName) {
-		// Reset drawing state before switching tools
-		this.resetDrawingState();
-	
-		// Unselect the previous tool, if there is one
-		if (this.selectedTool != null && this.selectedTool.hasOwnProperty("unselectTool")) {
-			this.selectedTool.unselectTool();
-		}
-	
-		// Rest of the selectTool method...
-		// Find and select the new tool
-		this.selectedTool = this.tools.find(tool => tool.name === toolName);
-		if (!this.selectedTool) {
-			console.error("Tool not found: " + toolName);
-			return;
-		}
-	
-		// Highlight the selected tool in the UI
-		var items = selectAll(".sideBarItem");
-		items.forEach(item => item.style('border', '0')); // Remove borders from all tools
-		select("#" + toolName + "sideBarItem").style("border", "2px solid blue");
-	
-		// Initialize the selected tool, if it has an init method
-		if (this.selectedTool.hasOwnProperty("init")) {
-			this.selectedTool.init();
-		}
-	
-		// Populate tool options, if available
-		if (this.selectedTool.hasOwnProperty("populateOptions")) {
-			this.selectedTool.populateOptions();
-		}
-	};
+        var toolName = this.id().split("sideBarItem")[0];
+        self.selectTool(toolName);
+        loadPixels();
+    };
 
+    var addToolIcon = function(icon, name) {
+        var sideBarItem = createDiv("<img src='" + icon + "'></div>");
+        sideBarItem.class("sideBarItem");
+        sideBarItem.id(name + "sideBarItem");
+        sideBarItem.parent("sidebar");
+        sideBarItem.mouseClicked(toolbarItemClick);
+    };
+
+    this.addTool = function(tool) {
+        if (!tool.hasOwnProperty("icon") || !tool.hasOwnProperty("name")) {
+            alert("Make sure your tool has both a name and an icon");
+        }
+        this.tools.push(tool);
+        addToolIcon(tool.icon, tool.name);
+        if (this.selectedTool == null) {
+            this.selectTool(tool.name);
+        }
+    };
+
+    this.resetDrawingState = function() {
+        stroke(0);
+        strokeWeight(1);
+    };
+
+    this.selectTool = function(toolName) {
+        this.resetDrawingState();
+        
+        if (this.selectedTool != null && this.selectedTool.hasOwnProperty("unselectTool")) {
+            this.selectedTool.unselectTool();
+        }
+
+        // Reset color selection if switching to a non-drawing tool
+        if (toolName !== "freehandTool" && toolName !== "lineToTool" && toolName !== "sprayCanTool" && toolName !== "mirrorDrawTool") {
+            this.colourPalette.resetColourSelection();
+        }
+
+        this.selectedTool = this.tools.find(tool => tool.name === toolName);
+        if (!this.selectedTool) {
+            console.error("Tool not found: " + toolName);
+            return;
+        }
+
+        var items = selectAll(".sideBarItem");
+        items.forEach(item => item.style('border', '0'));
+        select("#" + toolName + "sideBarItem").style("border", "2px solid blue");
+
+        if (this.selectedTool.hasOwnProperty("init")) {
+            this.selectedTool.init();
+        }
+
+        if (this.selectedTool.hasOwnProperty("populateOptions")) {
+            this.selectedTool.populateOptions();
+        }
+    };
 }
-
