fcca14ea900423effb034a8268f9931ddbabfffd
Slx24s <SalahAhmed1904@outlook.com>
2024-05-08 22:28:40 +0100

    modified colour Palette so that it has a function that properly selects default colour after new tool picked

--- colourPalette.js
+++ colourPalette.js
@@ -1,58 +1,70 @@
-//Displays and handles the colour palette.
+
+// Displays and handles the colour palette.
 function ColourPalette() {
-	//a list of web colour strings
-	this.colours = [
+    // a list of web color strings
+    this.colours = [
         "black", "silver", "gray", "white", "maroon", "red", "purple",
-		"orange", "pink", "fuchsia", "green", "lime", "olive", "yellow", "navy",
-		"blue", "teal", "aqua"
-	];
-	//make the start colour be black
-	this.selectedColour = "black";
+        "orange", "pink", "fuchsia", "green", "lime", "olive", "yellow", "navy",
+        "blue", "teal", "aqua"
+    ];
+    // make the start colour be black
+    this.selectedColour = "black";
 
-	var self = this;
+    var self = this;
 
-	var colourClick = function() {
-		//remove the old border
-		var current = select("#" + self.selectedColour + "Swatch");
-		current.style("border", "0");
+    var colourClick = function() {
+        // remove the old border
+        var current = select("#" + self.selectedColour + "Swatch");
+        current.style("border", "0");
 
-		//get the new colour from the id of the clicked element
-		var c = this.id().split("Swatch")[0];
+        // get the new colour from the id of the clicked element
+        var c = this.id().split("Swatch")[0];
 
-		//set the selected colour and fill and stroke
-		self.selectedColour = c;
-		fill(c);
-		stroke(c);
+        // set the selected colour and fill and stroke
+        self.selectedColour = c;
+        fill(c);
+        stroke(c);
 
-		//add a new border to the selected colour
-		this.style("border", "2px solid blue");
-	};
+        // add a new border to the selected colour
+        this.style("border", "2px solid blue");
+    };
 
-	//load in the colours
-	this.loadColours = function() {
-		//set the fill and stroke properties to be black at the start of the programme
-		//running
-		fill(this.colours[0]);
-		stroke(this.colours[0]);
+    // load in the colours
+    this.loadColours = function() {
+        fill(this.colours[0]);
+        stroke(this.colours[0]);
 
-		//for each colour create a new div in the html for the colourSwatches
-		for (var i = 0; i < this.colours.length; i++) {
-			var colourID = this.colours[i] + "Swatch";
+        for (var i = 0; i < this.colours.length; i++) {
+            var colourID = this.colours[i] + "Swatch";
+            var colourSwatch = createDiv();
+            colourSwatch.class("colourSwatches");
+            colourSwatch.id(colourID);
+            select(".colourPalette").child(colourSwatch);
+            select("#" + colourID).style("background-color", this.colours[i]);
+            colourSwatch.mouseClicked(colourClick);
+        }
+    };
 
-			// Add the swatch to the palette and set its background
-			// colour to be the colour value.
-			var colourSwatch = createDiv();
-			colourSwatch.class("colourSwatches");
+    // Adds method to reset the colour selection
+    this.resetColourSelection = function() {
+        // Remove the border from the currently selected swatch
+        var currentSwatch = select("#" + this.selectedColour + "Swatch");
+        if (currentSwatch) {
+            currentSwatch.style("border", "0");
+        }
 
-			colourSwatch.id(colourID);
+        // Reset to default color (black in this case)
+        this.selectedColour = "black";
+        fill(this.selectedColour);
+        stroke(this.selectedColour);
 
-			select(".colourPalette").child(colourSwatch);
-			select("#" + colourID).style("background-color", this.colours[i]);
-			colourSwatch.mouseClicked(colourClick);
-		}
+        // Optionally add border back to the default selected color swatch
+        var defaultSwatch = select("#" + this.selectedColour + "Swatch");
+        if (defaultSwatch) {
+            defaultSwatch.style("border", "2px solid blue");
+        }
+    };
 
-		select(".colourSwatches").style("border", "2px solid blue");
-	};
-	//call the loadColours function now it is declared
-	this.loadColours();
+    // Call the loadColours function now it is declared
+    this.loadColours();
 }
