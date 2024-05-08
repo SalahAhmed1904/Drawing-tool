0053bdab2b757c0b0ba5caeeca32cfcd24e151c8
Slx24s <SalahAhmed1904@outlook.com>
2024-05-08 15:33:20 +0100

    updated sprayCanTool to add option for spray density and ui management

--- sprayCanTool.js
+++ sprayCanTool.js
@@ -1,22 +1,65 @@
+// function sprayCanTool() {
+//     this.name = "sprayCanTool";
+//     this.icon = "assets/sprayCan.jpg";
+//     this.points = 13;
+//     this.spread = 10;
+//     this.pointSize = 1;  // Default size of each point
+//     var strokeWeightInput; // Input for adjusting the stroke weight
+//     var applyButton; // Button to apply stroke weight
+
+//     this.init = function() {
+//         // Select the input and button elements
+//         strokeWeightInput = select('#strokeInput');
+//         applyButton = select('#applyStroke');
+
+//         // Add event listener to the button
+//         applyButton.mousePressed(() => {
+//             const weight = parseFloat(strokeWeightInput.value());
+//             if (weight >= 0.1 && weight <= 10) {
+//                 strokeWeight(weight);
+//             } else {
+//                 alert("Please enter a value between 0.1 and 10");
+//             }
+//         });
+//     };
+
+//     this.draw = function() {
+//         if (mouseIsPressed) {
+//             for (var i = 0; i < this.points; i++) {
+//                 var x = random(mouseX - this.spread, mouseX + this.spread);
+//                 var y = random(mouseY - this.spread, mouseY + this.spread);
+//                 ellipse(x, y, this.pointSize, this.pointSize);  // Use ellipse for variable size
+//             }
+//         }
+//     };
+
+//     this.unselectTool = function() {
+//         if (strokeWeightInput) {
+//             strokeWeightInput.value(1); // Reset to default value when tool is unselected
+//         }
+//     };
+// }
+
+
 function sprayCanTool() {
     this.name = "sprayCanTool";
     this.icon = "assets/sprayCan.jpg";
     this.points = 13;
     this.spread = 10;
     this.pointSize = 1;  // Default size of each point
-    var strokeWeightInput; // Input for adjusting the stroke weight
-    var applyButton; // Button to apply stroke weight
 
-    this.init = function() {
-        // Select the input and button elements
-        strokeWeightInput = select('#strokeInput');
-        applyButton = select('#applyStroke');
+    this.populateOptions = function() {
+        let optionsArea = select('.options');
+        // Clear previous options and set up new ones for the spray can tool
+        optionsArea.html('<label for="sprayCanStrokeInput" class="slider-label">Spray Density (0.1 to 10):</label><input type="number" id="sprayCanStrokeInput" min="0.1" max="10" step="0.1" value="1"><button id="applySprayStroke">Apply</button>');
 
-        // Add event listener to the button
+        var applyButton = select('#applySprayStroke');
+        var strokeWeightInput = select('#sprayCanStrokeInput');
+
         applyButton.mousePressed(() => {
             const weight = parseFloat(strokeWeightInput.value());
             if (weight >= 0.1 && weight <= 10) {
-                strokeWeight(weight);
+                this.pointSize = weight;  // Adjust point size based on input
             } else {
                 alert("Please enter a value between 0.1 and 10");
             }
@@ -28,14 +71,14 @@ function sprayCanTool() {
             for (var i = 0; i < this.points; i++) {
                 var x = random(mouseX - this.spread, mouseX + this.spread);
                 var y = random(mouseY - this.spread, mouseY + this.spread);
-                ellipse(x, y, this.pointSize, this.pointSize);  // Use ellipse for variable size
+                ellipse(x, y, this.pointSize, this.pointSize);
             }
         }
     };
 
     this.unselectTool = function() {
-        if (strokeWeightInput) {
-            strokeWeightInput.value(1); // Reset to default value when tool is unselected
-        }
+        // Clear the options area when another tool is selected
+        select('.options').html('');
     };
 }
+
