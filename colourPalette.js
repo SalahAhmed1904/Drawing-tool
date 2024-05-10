805762d1493202191660671030df8e6d8636e650
Slx24s <SalahAhmed1904@outlook.com>
2024-05-10 03:02:31 +0100

    added custom rgb values for colour palette

--- colourPalette.js
+++ colourPalette.js
@@ -33,7 +33,7 @@ function ColourPalette() {
     this.loadColours = function() {
         fill(this.colours[0]);
         stroke(this.colours[0]);
-
+    
         for (var i = 0; i < this.colours.length; i++) {
             var colourID = this.colours[i] + "Swatch";
             var colourSwatch = createDiv();
@@ -43,7 +43,47 @@ function ColourPalette() {
             select("#" + colourID).style("background-color", this.colours[i]);
             colourSwatch.mouseClicked(colourClick);
         }
+        
+        // Create container for custom color button
+        var buttonContainer = createDiv();
+        buttonContainer.class("customColorButtonContainer");
+        select(".colourPalette").child(buttonContainer);
+        
+        // Add custom color button
+        var customColorButton = createButton('Custom Color');
+        customColorButton.parent(buttonContainer); // Append button inside buttonContainer
+        customColorButton.mouseClicked(function() {
+            self.promptForCustomColor();
+        });
     };
+
+    this.promptForCustomColor = function() {
+        var rgbString = prompt("Enter a custom color in RGB format (R, G, B):", "255, 0, 0");
+        if (rgbString) {
+            var rgbParts = rgbString.split(',').map(function(item) {
+                return parseInt(item.trim());
+            });
+    
+            if (rgbParts.length === 3 && rgbParts.every(function(value) { return !isNaN(value) && value >= 0 && value <= 255; })) {
+                var newColor = `rgb(${rgbParts[0]}, ${rgbParts[1]}, ${rgbParts[2]})`;
+                self.colours.push(newColor);
+                self.addColourSwatch(newColor);
+            } else {
+                alert("Invalid RGB values. Please enter values like 255, 0, 0.");
+            }
+        }
+    };
+
+    this.addColourSwatch = function(color) {
+        var colourID = color + "Swatch";
+        var colourSwatch = createDiv();
+        colourSwatch.class("colourSwatches");
+        colourSwatch.id(colourID);
+        select(".colourPalette").child(colourSwatch);
+        select("#" + colourID).style("background-color", color);
+        colourSwatch.mouseClicked(colourClick);
+    };
+
 
     // Adds method to reset the colour selection
     this.resetColourSelection = function() {
